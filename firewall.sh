10b6979d9a37fc87899425e0a595f66ad36137f7
netagevirtzer <neta.gev@campus.technion.ac.il>
2021-01-26 08:37:03 +0200

    delete spaces in the end

--- firewall.sh
+++ firewall.sh
@@ -2,13 +2,20 @@
 
 while read line
 do
+	echo "$line"| cat >> pkts.txt
+done
+while read rule
+do
 	packets=""
-	echo "$line"| cat >line.txt
+		
 	# remove empty lines, spaces and comments
-	sed -i 's/#.*//g;/^$/d;s/\s//g' line.txt
+	echo "$rule"| cat > rule.txt 
+	sed -i 's/#.*//g;/^$/d;s/\s//g' rule.txt
+
+
 
 	# save all rule fileds in array
-	rule=$(awk -F ',' '{print $1,$2,$3,$4}' line.txt)
+	rule=$(awk -F ',' '{print $1,$2,$3,$4}' rule.txt)
 	rules=($rule)
 
 	# check if the packet fits the rule
@@ -16,21 +23,24 @@ do
 
 		for i in "${rules[@]}"
 		do
-        	cat $1 | ./firewall.exe "$i" 2>/dev/null | cat >> packets.txt
+	        cat pkts.txt | ./firewall.exe "$i" 2>/dev/null | cat >> packets.txt
 		done
-
+		#counts the number of rules fields the packet fits
 		sort packets.txt | uniq -c > sortpack.txt
-		# findes packets which fits all of the rule fields 
+		#findes packets which fits all of the rule fields 
 		grep -E "^ *${#rules[@]}"  sortpack.txt| cat > Mp.txt
-		
-		sed -i "s/^ *${#rules[@]}//g;s/\s//g" Mp.txt 
+
+		sed -i "s/^ *${#rules[@]}//g" Mp.txt	
 		cat Mp.txt>>all_packets.txt
 		cat /dev/null > packets.txt
-		
+			
 	fi
+done < "$1"
 
-done
 
-sort all_packets.txt | uniq > Mp.txt
-cat Mp.txt
+
+sort all_packets.txt | uniq >> Mpl.txt
+# delete spaces and nuber of rules
+sed -i "s/\s//g" Mpl.txt 
+cat Mpl.txt
 rm *.txt
