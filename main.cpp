f62c678f320905de2f4afcabd826a376e1d8778e
OfirBenYosef <ofben.yosef@campus.technion.ac.il>
2021-01-26 11:27:15 +0200

    no more 80

--- main.cpp
+++ main.cpp
@@ -19,14 +19,16 @@ int main(int argc, char **argv){
     /* separate the field name from the rule */
     the_rule.split("=",&rule,&size);
     /* if the filed name fits to ip, check if the packets fits thr rule*/
-    if((rule[0].trim().equals("src-ip")) || (rule[0].trim().equals("dst-ip"))){
+    if((rule[0].trim().equals("src-ip")) || 
+        (rule[0].trim().equals("dst-ip"))){
         Ip ip_rule(the_rule.trim());
         ip_rule.set_value(rule[1].trim());
         parse_input(ip_rule);
 
     }
     /* if the filed name fits to port, check if the packets fits thr rule*/
-    if((rule[0].trim().equals("dst-port"))||(rule[0].trim().equals("src-port"))){
+    if((rule[0].trim().equals("dst-port"))||
+        (rule[0].trim().equals("src-port"))){
         Port port_rule(the_rule.trim());
         port_rule.set_value(rule[1].trim());
         parse_input(port_rule);
